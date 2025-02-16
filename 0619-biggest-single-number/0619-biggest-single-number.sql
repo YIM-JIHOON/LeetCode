cbd328cf53fbc9655ac7305322079f871960a9c8
YIM JIHOON <yhoon1710@gmail.com>
2025-02-16 22:06:26 +0900

    Time: 1253 ms (6.18%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,12 +1,10 @@
--- 가장 큰 단일 숫자 조회 (단일 숫자가 없으면 null을 출력)
+-- 가장 큰 단일값 조회 (단일 숫자가 없으면 null을 출력)
 
--- 단일 숫자만 조회
-WITH CTE AS (
+SELECT MAX(num) AS num  -- 가장 큰 단일값 조회
+FROM MyNumbers
+WHERE num IN (  -- 단일값만 필터 
     SELECT num
     FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(num) = 1 
-)
-
-SELECT MAX(num) AS num
-FROM CTE
+    HAVING COUNT(num) = 1
+    )
