019317a5afd58b5b0288286b77885793db0e0cd1
YIM JIHOON <yhoon1710@gmail.com>
2025-02-22 19:48:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,21 +1,9 @@
 -- 각 부서의 상위 3명의 고소득자 조회 (공동 순위 인정)
 
--- 직원 테이블에서 부서별 소득 높은 순으로 나열
-WITH SALARY_RANK AS (
-    SELECT
-        id,
-        name,
-        salary,
-        departmentId,
-        DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
-    FROM Employee
-)
--- 부서 테이블과 조인하고, 그룹별 갯수제한
-SELECT 
-    D.name AS Department,
-    R.name AS Employee,
-    R.salary AS Salary 
-FROM SALARY_RANK R
-JOIN Department D
-  ON R.departmentId = D.id
-WHERE R.salary_rank <= 3
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM (SELECT *, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS Salary_Rank
+      FROM Employee
+     ) e
+JOIN Department d 
+  ON e.departmentId = d.id
+WHERE e.Salary_Rank <= 3
