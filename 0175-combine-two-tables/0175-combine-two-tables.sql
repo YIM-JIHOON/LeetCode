62929d153cd20073155cf43aeb373d06730b19f0
YIM JIHOON <yhoon1710@gmail.com>
2025-03-06 21:35:37 +0900

    Time: 617 ms (74.31%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,10 +1,9 @@
--- 각 사람의 이름, 성, 도시, 주를 조회 (Address 테이블에 personId가 없으면 주소는 null로 출력)
-
-SELECT 
+-- 각 사람의 이름, 성, 도시, 주를 조회 (personId가 없으면 null 출력)
+SELECT
     P.firstName,
     P.lastName,
     A.city,
     A.state
 FROM Person P
 LEFT JOIN Address A
-       ON A.personId = P.personId
+       ON P.personId = A.personId
